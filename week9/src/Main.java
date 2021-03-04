70e0ff1b2b7de497bd24e71cca26a608e03b7869
linlaowee <78874287+linlaowee@users.noreply.github.com>
2021-03-04 18:46:13 +0100

    finished task two

--- week9/src/Main.java
+++ week9/src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner scan;
     private static String[][] text = new String[12][1];
+    private static String[][] splitarray = new String[12][1];
 
     public static void main(String[] args) throws FileNotFoundException {
 
@@ -18,16 +19,27 @@ public class Main {
             i++;
         }
 
+        File file2 = new File("data.txt");
+        scan = new Scanner(file2);
+        int j = 0;
+        while (scan.hasNextLine()) {
+            splitarray[j] = scan.nextLine().split("\\. ");
+
+            j++;
+
+        }
+
         // todo: print alle ord der starter med det der sendes med som argument,
-        printWordsStartingWith("Ø");
+        //printWordsStartingWith("Ø");
         //todo: print alle ord der har præcis det antal bogstaver der sendes med som argument (inkluderer IKKE tal og tegnene '.' og ',')
-        printWordsOfLength(3);
+        //printWordsOfLength(3);
 
 
         //test dine metoder ved at kalde dem her:
+        System.out.println("Words with double consonant:");
         printWordsWithDoubleConsonant();
-
-
+        System.out.println("Longest sentence:");
+        printLongestSentence();
 
 
     }
@@ -72,13 +84,13 @@ public class Main {
 
     //skriv dine metoder herunder:
 
-    private static void printWordsWithDoubleConsonant(){
+    private static void printWordsWithDoubleConsonant() {
         //boolean wordIsValid = true;
 
-        for (int i = 0; i < text.length; i++){
-            for (String s : text[i]){
-                for (int j = 0; j < s.length()-1; j++){
-                    if (s.charAt(j) == s.charAt(j+1) && isConsonant(s.charAt(j))){
+        for (int i = 0; i < text.length; i++) {
+            for (String s : text[i]) {
+                for (int j = 0; j < s.length() - 1; j++) {
+                    if (s.charAt(j) == s.charAt(j + 1) && isConsonant(s.charAt(j))) {
                         System.out.println(s);
                     }
                 }
@@ -87,15 +99,27 @@ public class Main {
         }
     }
 
-    private static boolean isConsonant(char c){
-        if ((c == 'a' ) || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u' ) || (Character.isDigit(c) == true)){
+    private static boolean isConsonant(char c) {
+        if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') || (Character.isDigit(c) == true)) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
 
 
+    private static void printLongestSentence(){
+        int maxLength = 0;
+        String longestString = null;
+        for (int j = 0; j < splitarray.length; j++) {
+            for (String s : splitarray[j]) {
+                if (s.length() > maxLength) {
+                    maxLength = s.length();
+                    longestString = s;
+                }
+            }
+        }
+        System.out.println(longestString);
+    }
 
 }
